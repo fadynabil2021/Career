d0528a0076661b0c93cd440889ecc432c24ebc05
Fady Nabil Mofeed <77495143+fadynabil2021@users.noreply.github.com>
2026-01-21 20:36:37 +0200

    Create config.py for application settings
    Add configuration settings for the application using Pydantic.

---
+++ Mini-Rag/App /src/helper /config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings , SettingsConfigDict
+
+class Settings(BaseSettings):
+
+    APP_NAME : str 
+    APP_VERSION : str
+    DEBUG : bool 
+    LOG_LEVEL : str
+    PORT : int 
+    OPEN_AI_API_KEY : str 
+    FILE_ALLOWED_TYPES : list 
+    FILE_ALLOWED_SIZE : int 
+    PROJECTS_DIR : str 
+    FILE_CHUNCK_SIZE : int 
+
+    class Config:
+        env_file=".env"
+
+
+def get_settings():
+    return Settings()
+    
